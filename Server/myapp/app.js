aca49bc634b22a0c2a7ed88e38fe37cc42009811
inrir <kimms5617@naver.com>
2023-03-19 23:43:07 +0900

    [FEAT] Sign Up

--- Server/myapp/app.js
+++ Server/myapp/app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const sign_up = require('./routes/api/member/sign_up');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+// Sign Up (input: email, email(check), pw, pw(check), phone_number, name)
+
+app.post('/API/Sign_up', (req, res) => {
+  console.log("[Call Sign up API]");
+
+  const userEmail = req.body.email;
+  const userEmailCheck = req.body.emailCheck;
+  const userPw = req.body.pw;
+  const userPwCheck = req.body.pwCheck;
+  const userPhone_number = req.body.phone_number;
+  const userName = req.body.name;
+
+  
+  if(sign_up.verification(userEmail, userEmailCheck, userPw, userPwCheck, userPhone_number, userName)){
+    res.json({status: res.statusCode});
+  }else{
+    console.log('error');
+    return res.send({error});
+  }  
+})
+
+// Log in | Sign in(input: email, pw) 
+
+
+
+
+// edit account(input: pw, phone_number, name)
+
+
+
+// session 도입하여 삭제.
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
