047db9532ef212c6011915e20a2ef98340253be2
AhnDo0 <i0796075@gmail.com>
2023-03-31 20:16:09 +0900

    [FEAT] 비밀번호 찾기 함수 추가

--- Client/components/FindPWScreen.js
+++ Client/components/FindPWScreen.js
@@ -2,19 +2,38 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import Header from './Header';
-// import apiClient from './api';
 
 const FindPWScreen = () => {
   const [email, setEmail] = useState('');
+  const SERVER_URL = 'http://ec2-43-200-5-132.ap-northeast-2.compute.amazonaws.com:3000';
 
   const handleForgotPassword = async () => {
-    // try {
-    //   // 이메일로 비밀번호 정보를 보내는 API 호출을 여기에 구현해야 함
-    //   await apiClient.resetPassword(email);
-    //   Alert.alert('비밀번호 찾기', '비밀번호 정보가 이메일로 전송되었습니다.');
-    // } catch (error) {
-    //   Alert.alert('에러', '비밀번호 찾기 과정에서 문제가 발생했습니다.');
-    // }
+    try{
+      const response = await fetch('${SERVER_URL}/API/Search_pw',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email:email,
+        }),
+      });
+
+      const data = response.json();
+
+      if(data.pw){
+        //비밀번호 찾기 성공, 새 비밀번호 전송 완료
+        console.log('새 비밀번호 전송');
+        Alert.alert('성공', '이메일로 비밀번호 정보가 전송되었습니다. 확인해주세요.');
+      }else{
+        //비밀번호 찾기 실패, 이메일 찾지 못함
+        console.log('비밀번호 찾기 실패');
+        Alert.alert('실패', '입력하신 이메일이 존재하지 않습니다. 다시 확인해주세요.');
+      }
+    }catch(error){
+      console.error('비밀번호 찾기 도중 오류 발생', error);
+      Alert.alert('오류', '비밀번호 찾기 도중 오류가 발생했습니다. 다시 시도해주세요.');
+    }
   };
 
   return (
